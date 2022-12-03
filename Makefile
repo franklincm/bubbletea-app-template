3536b0ebe2feb79a6a20fba429d13d337f45ec1a
Chase Franklin <cfranklin@nuvalence.io>
2022-12-02 21:43:31 -0600

    layout

---
+++ Makefile
@@ -0,0 +1,26 @@
+
+.DEFAULT_GOAL := build
+
+fmt:
+	gofmt -w -s .
+.PHONY:fmt
+
+lint: fmt
+	golint .
+.PHONY:lint
+
+vet: fmt
+	go vet ./...
+.PHONY:vet
+
+build: vet
+	go build -o dist/c9s main.go
+.PHONY:build
+
+test:
+	go test
+.PHONY:test
+
+clean:
+	$(RM) -rf dist/
+.PHONY:clean
--- main.go
+++ main.go
@@ -3,45 +3,87 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/charmbracelet/bubbles/key"
-	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/bubbles/stopwatch"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
-type errMsg error
+var borderKeys = key.NewBinding(
+	key.WithKeys("b", "ctrl+b"),
+	key.WithHelp("", "toggle border"),
+)
 
-type model struct {
-	spinner  spinner.Model
-	quitting bool
-	err      error
-}
+var headerStyle = lipgloss.NewStyle().
+	Margin(0).
+	Padding(0).
+	BorderBottom(true).
+	BorderTop(false).
+	BorderLeft(false).
+	BorderRight(false).
+	BorderStyle(lipgloss.NormalBorder())
+
+var testStyle = lipgloss.NewStyle()
+var fullscreenStyle = lipgloss.NewStyle().
+	Padding(0).
+	Margin(0).
+	Align(lipgloss.Center)
 
 var quitKeys = key.NewBinding(
 	key.WithKeys("q", "esc", "ctrl+c"),
 	key.WithHelp("", "press q to quit"),
 )
 
-func initialModel() model {
-	s := spinner.New()
-	s.Spinner = spinner.Dot
-	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
-	return model{spinner: s}
+type errMsg error
+
+type Model struct {
+	quitting  bool
+	err       error
+	textStyle lipgloss.Style
+	width     int
+	height    int
+	ready     bool
+	stopwatch stopwatch.Model
+	border    bool
 }
 
-func (m model) Init() tea.Cmd {
-	return m.spinner.Tick
+func NewModel() Model {
+	style := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("black")).
+		Background(lipgloss.Color("240")).
+		Align(lipgloss.Center)
+
+	stopwatch := stopwatch.NewWithInterval(time.Millisecond)
+
+	return Model{
+		textStyle: style,
+		stopwatch: stopwatch,
+		border:    false,
+	}
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m Model) Init() tea.Cmd {
+	return tea.Batch(m.stopwatch.Init(), tea.EnterAltScreen)
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 
+	case tea.WindowSizeMsg:
+		fullscreenStyle.Width(msg.Width)
+		fullscreenStyle.Height(msg.Height)
+		m.width = msg.Width
+		m.height = msg.Height
+		return m, nil
 	case tea.KeyMsg:
 		if key.Matches(msg, quitKeys) {
 			m.quitting = true
 			return m, tea.Quit
 
+		} else if key.Matches(msg, borderKeys) {
+			m.border = !m.border
 		}
 		return m, nil
 	case errMsg:
@@ -50,24 +92,48 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	default:
 		var cmd tea.Cmd
-		m.spinner, cmd = m.spinner.Update(msg)
+		m.stopwatch, cmd = m.stopwatch.Update(msg)
 		return m, cmd
 	}
 }
 
-func (m model) View() string {
+func (m Model) View() string {
 	if m.err != nil {
 		return m.err.Error()
 	}
-	str := fmt.Sprintf("\n\n   %s Loading forever... %s\n\n", m.spinner.View(), quitKeys.Help().Desc)
+
+	str := fmt.Sprintf(
+		"%-*s %*s %*s",
+		20,
+		"c9s",
+		(m.width-20-12)/2,
+		fmt.Sprintf("width:%d", (m.width-20-15)/2),
+		(m.width-20-12)/2,
+		quitKeys.Help().Desc,
+	)
+
 	if m.quitting {
 		return str + "\n"
 	}
-	return str
+
+	testStr := `
+	oh
+	hai
+	wut
+	up
+	`
+	return fullscreenStyle.Height(m.height).Render(
+		lipgloss.JoinVertical(
+			lipgloss.Top,
+			headerStyle.Width(m.width).Render(str),
+			testStyle.Height(m.height-5).Width(m.width).Margin(0).Padding(0).BorderStyle(lipgloss.NormalBorder()).BorderTop(false).BorderBottom(true).Render(testStr),
+			testStyle.Width(m.width).Margin(0).Padding(0).BorderStyle(lipgloss.NormalBorder()).BorderTop(false).BorderBottom(false).Render(fmt.Sprintf("%s:%d", "footer, height", m.height)),
+		),
+	)
 }
 
 func main() {
-	p := tea.NewProgram(initialModel())
+	p := tea.NewProgram(NewModel(), tea.WithAltScreen())
 	if err := p.Start(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
